0417ddb9ec7968c52adc8aa33891a290414a95c6
JabborovEgamberdi <jabborovegamberdi2002@gmail.com>
2022-08-10 00:45:20 +0500

    some classes updated

--- src/main/java/com/example/itmasters/controller/UserController.java
+++ src/main/java/com/example/itmasters/controller/UserController.java
@@ -9,10 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,26 +20,31 @@ public class UserController {
 
     private final UserService userService;
 
+    @GetMapping
     public HttpEntity<?> getAllUsers() {
         List<User> allUsers = userService.getAllUsers();
         return new ResponseEntity<>(allUsers, HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
     public HttpEntity<?> getUserById(@PathVariable Integer id) {
         User userById = userService.getUserById(id);
         return new ResponseEntity<>(userById, HttpStatus.OK);
     }
 
+    @PostMapping("/save")
     public HttpEntity<?> addUser(@RequestBody UserDTO userDTO) {
         ApiResponse addUser = userService.addUser(userDTO);
         return new ResponseEntity<>(addUser, HttpStatus.ACCEPTED);
     }
 
+    @PutMapping("/update/{id}")
     public HttpEntity<?> updateUser(@PathVariable Integer id, @RequestBody UserDTO userDTO) {
         ApiResponse updateUser = userService.updateUser(id, userDTO);
         return new ResponseEntity<>(updateUser.isSuccess() ? HttpStatus.OK : HttpStatus.CONFLICT);
     }
 
+    @DeleteMapping("/delete/{id}")
     public HttpEntity<?> deleteUserById(@PathVariable Integer id) {
         ApiResponse deleteUserByID = userService.deleteUserByID(id);
         return new ResponseEntity<>(deleteUserByID.isSuccess() ? HttpStatus.OK : HttpStatus.CONFLICT);
