a00a4b19963059512fb34ce9e0c3ff51269fb8bf
JabborovEgamberdi <jabborovegamberdi2002@gmail.com>
2022-08-21 21:42:54 +0500

    some classes updated

--- src/main/java/com/example/itmasters/repository/DirectionForVacancyRepository.java
+++ src/main/java/com/example/itmasters/repository/DirectionForVacancyRepository.java
@@ -3,7 +3,9 @@ package com.example.itmasters.repository;
 
 import com.example.itmasters.entity.DirectionForVacancy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DirectionForVacancyRepository extends JpaRepository<DirectionForVacancy, Integer> {
 
 }
