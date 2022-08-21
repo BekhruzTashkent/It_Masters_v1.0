1f47e4f40bb9dc007a5b92a6a7cdf2f4db08584c
JabborovEgamberdi <jabborovegamberdi2002@gmail.com>
2022-08-21 21:43:22 +0500

    some classes updated

--- src/main/java/com/example/itmasters/repository/JobForUserRepository.java
+++ src/main/java/com/example/itmasters/repository/JobForUserRepository.java
@@ -3,7 +3,9 @@ package com.example.itmasters.repository;
 import com.example.itmasters.entity.JobForUser;
 import com.example.itmasters.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JobForUserRepository extends JpaRepository<JobForUser, Integer> {
 
 }
