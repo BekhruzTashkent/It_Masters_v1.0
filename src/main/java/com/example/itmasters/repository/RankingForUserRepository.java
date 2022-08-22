5ed782cdb72f30eb9e1ddb319d9d3e0449e182df
JabborovEgamberdi <jabborovegamberdi2002@gmail.com>
2022-08-22 22:21:00 +0500

    some classes updated

--- src/main/java/com/example/itmasters/repository/RankingForUserRepository.java
+++ src/main/java/com/example/itmasters/repository/RankingForUserRepository.java
@@ -3,7 +3,9 @@ package com.example.itmasters.repository;
 import com.example.itmasters.entity.DirectionForUser;
 import com.example.itmasters.entity.RankingForUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RankingForUserRepository extends JpaRepository<RankingForUser, Integer> {
 
 }
