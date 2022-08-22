29fff735d5b911775809f7ab197dc93b8e84efe0
JabborovEgamberdi <jabborovegamberdi2002@gmail.com>
2022-08-22 22:21:27 +0500

    some classes updated

--- src/main/java/com/example/itmasters/repository/UserRepository.java
+++ src/main/java/com/example/itmasters/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.itmasters.repository;
 
 import com.example.itmasters.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
