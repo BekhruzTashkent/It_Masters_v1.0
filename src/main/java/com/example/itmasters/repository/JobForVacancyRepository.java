b3955ae9e587cec1ad126d1ef2713cce30aad0ad
JabborovEgamberdi <jabborovegamberdi2002@gmail.com>
2022-10-17 17:30:29 +0500

    classes updated

--- src/main/java/com/example/itmasters/repository/JobForVacancyRepository.java
+++ src/main/java/com/example/itmasters/repository/JobForVacancyRepository.java
@@ -1,6 +1,5 @@
 package com.example.itmasters.repository;
 
-import com.example.itmasters.entity.JobForUser;
 import com.example.itmasters.entity.JobForVacancy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
