464e4768ccbf2eaa24264c53ea26d216562912dc
JabborovEgamberdi <jabborovegamberdi2002@gmail.com>
2022-10-22 23:13:46 +0500

    classes updated

--- src/main/java/com/example/itmasters/entity/JobForVacancy.java
+++ src/main/java/com/example/itmasters/entity/JobForVacancy.java
@@ -1,12 +1,10 @@
 package com.example.itmasters.entity;
 
 import com.example.itmasters.entity.template.AbsIntegerIDEntity;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.sql.Timestamp;
 
