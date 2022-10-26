bb54cc0d86f239da88bc5f02c67a859f06c1cb9a
JabborovEgamberdi <jabborovegamberdi2002@gmail.com>
2022-10-26 18:38:30 +0500

    classes updated

--- src/main/java/com/example/itmasters/Application.java
+++ src/main/java/com/example/itmasters/Application.java
@@ -2,8 +2,12 @@ package com.example.itmasters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.itmasters.entity")
+@EnableJpaRepositories(basePackages = "com.example.itmasters")
 public class Application {
 
     public static void main(String[] args) {
